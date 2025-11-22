0f30f7f0f7ad8aee512f0bf38635a597b6113385
Edoardo <sarriedoardo24@gmail.com>
2025-11-22 20:21:03 +0100

    services distribution

--- exec/analyze_traces.sh
+++
@@ -1,9 +0,0 @@
-echo "extracting data from traces..."
-cd E2E-analysis/analize_traces
-kubectl cp analyzer:data/traces.json traces.json
-source /opt/miniconda3/etc/profile.d/conda.sh
-conda activate car-rental
-python main.py > statistics.txt
-conda deactivate
-rm -r __pycache__/
-cd ../..
